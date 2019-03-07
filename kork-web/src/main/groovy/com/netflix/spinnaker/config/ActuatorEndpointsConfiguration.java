b326019fdeb30c4cd8d43dc07076ac18c8ab0f80
Emily Burns <emburns@netflix.com>
2019-03-06 21:01:19 -0800

    chore(boot2): change order of ActuatorEndpointsConfiguration (#243)

--- kork-web/src/main/groovy/com/netflix/spinnaker/config/ActuatorEndpointsConfiguration.java
+++ kork-web/src/main/groovy/com/netflix/spinnaker/config/ActuatorEndpointsConfiguration.java
@@ -17,6 +17,7 @@ package com.netflix.spinnaker.config;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -24,6 +25,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class ActuatorEndpointsConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
+  @Order(1000)
   public void configure(HttpSecurity http) throws Exception {
     // The health endpoint should always be exposed without auth.
     http.authorizeRequests().requestMatchers(EndpointRequest.to("health")).permitAll();
