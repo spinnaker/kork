e882625888252cb0d97e1868632b2e7296e5782a
Cameron Fieber <cfieber@netflix.com>
2019-04-18 22:07:03 -0700

    fix(authz): apply requestMatchers on security configurers.
    Without registering a requestMatcher, these WebSecurityConfigurerAdapters will handle the auth
    for any request, when they really only care about allowing access to certain paths and the
    opinions of how to perform application wide authentication/authorization are intended to be
    configured elsewhere.

--- kork-web/src/main/groovy/com/netflix/spinnaker/config/ActuatorEndpointsConfiguration.java
+++ kork-web/src/main/groovy/com/netflix/spinnaker/config/ActuatorEndpointsConfiguration.java
@@ -16,18 +16,20 @@
 package com.netflix.spinnaker.config;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Order(1000)
+@Order(Ordered.HIGHEST_PRECEDENCE + 67)
 public class ActuatorEndpointsConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
     // The health endpoint should always be exposed without auth.
-    http.authorizeRequests().requestMatchers(EndpointRequest.to("health")).permitAll();
+    http.requestMatcher(EndpointRequest.to(HealthEndpoint.class)).authorizeRequests().anyRequest().permitAll();
   }
 }
