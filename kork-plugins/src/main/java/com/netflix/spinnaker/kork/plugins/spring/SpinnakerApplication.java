215df6a3648381fbbe30c5257f081f3f06848aea
Cameron Motevasselani <cameron.motevasselani@armory.io>
2019-06-21 16:05:11 -0700

    feat(plugin): plugin-loader

---
+++ kork-plugins/src/main/java/com/netflix/spinnaker/kork/plugins/spring/SpinnakerApplication.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 Armory, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kork.plugins.spring;
+
+import java.util.Map;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class SpinnakerApplication extends SpringBootServletInitializer {
+
+  /**
+   * Projects that want to support plugins should extend SpinnakerApplication instead of
+   * SpringBootServletInitializer. This method wraps the call to SpringApplicationBuilder after
+   * loading plugins
+   *
+   * @param defaultProps Properties to pass to spring application
+   * @param source The parent class loader for delegation
+   * @param args The application arguments (usually passed from a Java main method)
+   */
+  public static void initialize(Map<String, Object> defaultProps, Class source, String... args) {
+    new PluginLoader().loadPlugins(source);
+    new SpringApplicationBuilder().properties(defaultProps).sources(source).run(args);
+  }
+}
