beb6958265b935eb5259c7aa0d4fff11e5a691c0
Sairam Rekapalli <srekapalli@netflix.com>
2019-11-26 17:40:31 -0800

    feat(sql): Support for multiple SQL data sources

--- kork-sql/src/test/kotlin/com/netflix/spinnaker/kork/sql/SqlConfigurationTests.kt
+++ kork-sql/src/test/kotlin/com/netflix/spinnaker/kork/sql/SqlConfigurationTests.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2018 Netflix, Inc.
+ * Copyright 2019 Netflix, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,67 +13,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.netflix.spinnaker.kork.sql
 
 import com.netflix.spinnaker.kork.PlatformComponents
 import com.netflix.spinnaker.kork.sql.config.DefaultSqlConfiguration
-import com.netflix.spinnaker.kork.sql.health.SqlHealthIndicator
+import com.netflix.spinnaker.kork.sql.config.SqlProperties
 import org.jooq.DSLContext
-import org.jooq.impl.DSL.field
-import org.jooq.impl.DSL.table
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.getBeansOfType
-import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import strikt.api.expectThat
-import strikt.assertions.isA
 import strikt.assertions.isEqualTo
 import strikt.assertions.isNotNull
 
 @RunWith(SpringRunner::class)
+@ActiveProfiles("test")
 @SpringBootTest(
-  classes = [StartupTestApp::class],
-  properties = [
-    "sql.enabled=true",
-    "sql.migration.jdbcUrl=jdbc:h2:mem:test",
-    "sql.migration.dialect=H2",
-    "sql.connectionPool.jdbcUrl=jdbc:h2:mem:test",
-    "sql.connectionPool.dialect=H2"
-  ]
+  classes = [SqlConfigTestApp::class]
 )
-internal class SpringStartupTests {
-
-  @Autowired
-  lateinit var dbHealthIndicator: HealthIndicator
-
-  @Autowired
-  lateinit var jooq: DSLContext
+internal class SqlConfigurationTests {
 
   @Autowired
   lateinit var applicationContext: ApplicationContext
 
+  @Autowired
+  lateinit var sqlProperties: SqlProperties
+
   @Test
-  fun `uses SqlHealthIndicator`() {
-    expectThat(dbHealthIndicator).isA<SqlHealthIndicator>()
-
-    expectThat(
-      jooq
-        .insertInto(table("healthcheck"), listOf(field("id")))
-        .values(true).execute()
-    ).isEqualTo(1)
-
-    expectThat(applicationContext.getBeansOfType(DSLContext::class.java).size).isEqualTo(1)
+  fun `should have 2 JOOQ configured for both H2 & MYSQL pool`() {
+    expectThat(applicationContext.getBeansOfType(DSLContext::class.java).size).isEqualTo(2)
+    expectThat(sqlProperties.connectionPools.size).isEqualTo(2)
     expectThat(applicationContext.getBean("jooq")).isNotNull()
+    expectThat(applicationContext.getBean("secondaryJooq")).isNotNull()
     expectThat(applicationContext.getBean("liquibase")).isNotNull()
+    expectThat(applicationContext.getBean("secondaryLiquibase")).isNotNull()
   }
 }
 
 @SpringBootApplication
 @Import(PlatformComponents::class, DefaultSqlConfiguration::class)
-internal class StartupTestApp
+internal class SqlConfigTestApp
