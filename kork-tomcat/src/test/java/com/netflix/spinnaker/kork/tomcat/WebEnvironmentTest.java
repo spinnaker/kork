42a6f372ea0669c06f8a5c82a4a039ddb3f52eda
David Byron <dbyron@salesforce.com>
2021-05-04 12:20:06 -0700

    feat(tomcat): introduce default.rejectIllegalHeader configuration property
    The default is to behave as before this commit, which is to leave tomcat's default
    behavior.  In version 9.0.31 of tomcat, which spring boot 2.2.5 uses, tomcat rejects
    illegal http headers by default.  See
    https://github.com/apache/tomcat/blob/9.0.31/java/org/apache/coyote/http11/AbstractHttp11Protocol.java#L148.
    
    Note that only server.port and default.apiPort respect this configuration property.
    default.legacyServerPort does not.

--- kork-tomcat/src/test/java/com/netflix/spinnaker/kork/tomcat/WebEnvironmentTest.java
+++ kork-tomcat/src/test/java/com/netflix/spinnaker/kork/tomcat/WebEnvironmentTest.java
@@ -24,8 +24,17 @@ import org.springframework.web.util.UriComponentsBuilder;
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     classes = {WebEnvironmentTest.TestControllerConfiguration.class})
+// It would be lovely to have a per-method TestPropertySource annotation, or
+// some other simple way to parametrize tests to verify what happens when
+// default.rejectIllegalHeader isn't set at all, and set to true, in addition to
+// setting it to false.  At the moment this doesn't seem worth the code
+// duplication / complexity.  See
+// https://github.com/spring-projects/spring-framework/issues/18951.
 @TestPropertySource(
-    properties = {"logging.level.org.apache.coyote.http11.Http11InputBuffer = DEBUG"})
+    properties = {
+      "logging.level.org.apache.coyote.http11.Http11InputBuffer = DEBUG",
+      "default.rejectIllegalHeader = false"
+    })
 class WebEnvironmentTest {
 
   @LocalServerPort int port;
@@ -47,7 +56,7 @@ class WebEnvironmentTest {
 
     ResponseEntity<String> entity =
         restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(headers), String.class);
-    assertEquals(HttpStatus.BAD_REQUEST, entity.getStatusCode());
+    assertEquals(HttpStatus.OK, entity.getStatusCode());
   }
 
   @SpringBootApplication
