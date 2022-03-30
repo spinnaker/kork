718c71dfc67641fe1e6c4cc0885518e03f874ba2
Jørgen Jervidalo <jorgen.jervidalo@schibsted.com>
2022-03-31 00:03:57 +0200

    Don't overwrite existing disallowed fields

--- kork-web/src/main/java/com/netflix/spinnaker/config/BinderControllerAdvice.java
+++ kork-web/src/main/java/com/netflix/spinnaker/config/BinderControllerAdvice.java
@@ -1,5 +1,9 @@
 package com.netflix.spinnaker.config;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,7 +20,11 @@ public class BinderControllerAdvice {
 
   @InitBinder
   public void setAllowedFields(WebDataBinder dataBinder) {
-    String[] denylist = new String[] {"class.*", "Class.*", "*.class.*", "*.Class.*"};
-    dataBinder.setDisallowedFields(denylist);
+    Set<String> disallowedFields =
+        Optional.ofNullable(dataBinder.getDisallowedFields())
+            .map(existingFields -> new HashSet<>(Arrays.asList(existingFields)))
+            .orElse(new HashSet<>());
+    disallowedFields.addAll(Arrays.asList("class.*", "Class.*", "*.class.*", "*.Class.*"));
+    dataBinder.setDisallowedFields(disallowedFields.toArray(new String[0]));
   }
 }
