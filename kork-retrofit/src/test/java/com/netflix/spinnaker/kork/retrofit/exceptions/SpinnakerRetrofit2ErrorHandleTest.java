412d1e05bb498dd6f73c0585344aeca3a6935cdb
Pranav-b-7 <pranav.bhaskaran@opsmx.com>
2023-10-20 15:56:16 +0530

    test(retrofit2): Modified testNonJsonHttpErrorResponse to assert more values like url, response code, reason and message

--- kork-retrofit/src/test/java/com/netflix/spinnaker/kork/retrofit/exceptions/SpinnakerRetrofit2ErrorHandleTest.java
+++ kork-retrofit/src/test/java/com/netflix/spinnaker/kork/retrofit/exceptions/SpinnakerRetrofit2ErrorHandleTest.java
@@ -48,6 +48,8 @@ public class SpinnakerRetrofit2ErrorHandleTest {
 
   private static String responseBodyString;
 
+  private static String baseUrl = mockWebServer.url("/").toString();
+
   @BeforeAll
   public static void setupOnce() throws Exception {
 
@@ -58,7 +60,7 @@ public class SpinnakerRetrofit2ErrorHandleTest {
 
     retrofit2Service =
         new Retrofit.Builder()
-            .baseUrl(mockWebServer.url("/").toString())
+            .baseUrl(baseUrl)
             .client(
                 new OkHttpClient.Builder()
                     .callTimeout(1, TimeUnit.SECONDS)
@@ -197,14 +199,21 @@ public class SpinnakerRetrofit2ErrorHandleTest {
   public void testNonJsonHttpErrorResponse() {
 
     String invalidJsonTypeResponseBody = "{'errorResponse': 'Failure'";
+    int responseCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
+    String url = baseUrl + "retrofit2";
+    String reason = "Server Error";
 
     mockWebServer.enqueue(
-        new MockResponse()
-            .setResponseCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
-            .setBody(invalidJsonTypeResponseBody));
+        new MockResponse().setResponseCode(responseCode).setBody(invalidJsonTypeResponseBody));
     SpinnakerHttpException spinnakerHttpException =
         assertThrows(SpinnakerHttpException.class, () -> retrofit2Service.getRetrofit2().execute());
     assertNull(spinnakerHttpException.getResponseBody());
+    assertEquals(responseCode, spinnakerHttpException.getResponseCode());
+    assertEquals(
+        "Status: " + responseCode + ", URL: " + url + ", Message: " + reason,
+        spinnakerHttpException.getMessage());
+    assertEquals(url, spinnakerHttpException.getUrl());
+    assertEquals(reason, spinnakerHttpException.getReason());
   }
 
   interface DummyWithExecute {
