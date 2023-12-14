8f6fd2d79f10c19b78715f5c67b2f1e0443e2506
Matt Sicker <msicker@apple.com>
2023-12-14 12:22:14 -0600

    fix(credentials): Use lazy loading of CredentialsNavigator beans

--- kork-credentials/src/main/java/com/netflix/spinnaker/credentials/service/CredentialsInspector.java
+++ kork-credentials/src/main/java/com/netflix/spinnaker/credentials/service/CredentialsInspector.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,19 +19,16 @@ package com.netflix.spinnaker.credentials.service;
 import com.netflix.spinnaker.credentials.CredentialsError;
 import com.netflix.spinnaker.credentials.CredentialsView;
 import com.netflix.spinnaker.credentials.definition.CredentialsDefinition;
-import com.netflix.spinnaker.credentials.definition.CredentialsDefinitionRepository;
-import com.netflix.spinnaker.credentials.definition.CredentialsDefinitionSource;
 import com.netflix.spinnaker.credentials.definition.CredentialsNavigator;
 import com.netflix.spinnaker.credentials.validator.CredentialsDefinitionErrorCode;
 import com.netflix.spinnaker.security.AccessControlled;
 import com.netflix.spinnaker.security.Authorization;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.security.access.PermissionEvaluator;
@@ -40,33 +37,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Log4j2
+@RequiredArgsConstructor
 public class CredentialsInspector {
-  private final List<CredentialsNavigator<? extends CredentialsDefinition>> navigators;
+  // as this class is instantiated before all credentials-related beans have been defined, this must
+  // use lazy loading of beans via ObjectProvider instead of a List or similar collection
+  private final ObjectProvider<CredentialsNavigator<? extends CredentialsDefinition>> navigators;
   private final PermissionEvaluator permissionEvaluator;
-
-  public CredentialsInspector(
-      ObjectProvider<CredentialsDefinitionSource<? extends CredentialsDefinition>> sources,
-      PermissionEvaluator permissionEvaluator) {
-    List<CredentialsNavigator<? extends CredentialsDefinition>> navigators = new ArrayList<>();
-    for (CredentialsDefinitionSource<? extends CredentialsDefinition> source : sources) {
-      if (source instanceof CredentialsNavigator<?>) {
-        CredentialsNavigator<? extends CredentialsDefinition> navigator =
-            (CredentialsNavigator<? extends CredentialsDefinition>) source;
-        navigators.add(navigator);
-      } else {
-        log.warn("Unable to navigate credentials source {}", source.getClass());
-      }
-    }
-    this.navigators = Collections.unmodifiableList(navigators);
-    this.permissionEvaluator = permissionEvaluator;
-  }
 
   /**
    * Enumerates and inspects all credential definitions the given authenticated user has access to.
-   * This combines all {@link CredentialsDefinitionRepository} and {@link
-   * CredentialsDefinitionSource} definitions. The resulting list of credentials should be checked
-   * for duplicate account names. Credentials with permissions defined should be filtered in or out
-   * of the resulting list depending on whether the user has {@link Authorization#READ} permission.
+   * This combines all {@link CredentialsNavigator} beans to iterate over credentials definitions.
+   * The resulting list of credentials is checked for duplicate account names. Credentials with
+   * permissions defined are filtered in or out of the resulting list depending on whether the user
+   * has {@link Authorization#READ} permission.
    *
    * @param auth the authenticated user to inspect credentials for
    * @return a list of views into the inspected credentials
