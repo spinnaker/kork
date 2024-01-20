f6ffeb9fffcad23855c3a387c6cc465edf42bbd0
David Byron <dbyron@salesforce.com>
2024-01-19 23:35:39 -0800

    refactor(web/test): convert SpinnakerRequestHeaderInterceptorTest to use wiremock
    and real requests, instead of static mocking of AuthenticatedRequest since it seems like a
    more thorough test.  Matching the structure of SpinnakerRequestInterceptorTest also seems like
    a win.

--- kork-web/src/test/java/com/netflix/spinnaker/okhttp/SpinnakerRequestHeaderInterceptorTest.java
+++ kork-web/src/test/java/com/netflix/spinnaker/okhttp/SpinnakerRequestHeaderInterceptorTest.java
@@ -16,65 +16,118 @@
 
 package com.netflix.spinnaker.okhttp;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder;
 import com.netflix.spinnaker.kork.common.Header;
 import com.netflix.spinnaker.security.AuthenticatedRequest;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import okhttp3.Interceptor;
-import okhttp3.Request;
+import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
 
 class SpinnakerRequestHeaderInterceptorTest {
 
-  MockedStatic<AuthenticatedRequest> mockAuthenticatedRequest;
+  public static final String REQUEST_PATH = "/foo";
+
+  public static final String TEST_USER = "some-user";
+
+  public static final String TEST_ACCOUNTS = "some-accounts";
+
+  public static final String TEST_REQUEST_ID = "some-request-id";
+
+  public static final Map<Header, String> TEST_SPINNAKER_HEADERS =
+      Map.of(
+          Header.USER,
+          TEST_USER,
+          Header.ACCOUNTS,
+          TEST_ACCOUNTS,
+          Header.REQUEST_ID,
+          TEST_REQUEST_ID);
+
+  /**
+   * Use this instead of annotating the class with @WireMockTest so there's a WireMock object
+   * available for parameterized tests. Otherwise the arguments from e.g. ValueSource compete with
+   * the WireMockRuntimeInfo argument that @WireMockTest provides.
+   */
+  @RegisterExtension
+  static WireMockExtension wireMock =
+      WireMockExtension.newInstance().options(wireMockConfig().dynamicPort()).build();
 
   @BeforeEach
-  void setup() {
-    mockAuthenticatedRequest = Mockito.mockStatic(AuthenticatedRequest.class);
+  void setup(TestInfo testInfo, WireMockRuntimeInfo wmRuntimeInfo) {
+    System.out.println("--------------- Test " + testInfo.getDisplayName());
+
+    // set up an arbitrary response to avoid 404s, and so it's possible to
+    // verify the request headers wiremock receives.
+    wireMock.stubFor(get(REQUEST_PATH).willReturn(ok()));
   }
 
   @AfterEach
   void cleanup() {
-    mockAuthenticatedRequest.close();
+    AuthenticatedRequest.clear();
   }
 
-  @Test
-  void requestContainsAuthorizationHeader() throws Exception {
-    // given
-    Map<String, Optional<String>> authHeaders = new HashMap<String, Optional<String>>() {};
-    authHeaders.put(Header.USER.getHeader(), Optional.of("some user"));
-    authHeaders.put(Header.ACCOUNTS.getHeader(), Optional.of("Some ACCOUNTS"));
+  @ParameterizedTest(name = "propagateSpinnakerHeaders = {0}")
+  @ValueSource(booleans = {false, true})
+  void propagateSpinnakerHeaders(boolean propagateSpinnakerHeaders) throws Exception {
     OkHttpClientConfigurationProperties okHttpClientConfigurationProperties =
         new OkHttpClientConfigurationProperties();
-    SpinnakerRequestHeaderInterceptor headerInterceptor =
+    okHttpClientConfigurationProperties.setPropagateSpinnakerHeaders(propagateSpinnakerHeaders);
+    SpinnakerRequestHeaderInterceptor spinnakerRequestHeaderInterceptor =
         new SpinnakerRequestHeaderInterceptor(okHttpClientConfigurationProperties);
-    Interceptor.Chain chain = mock(Interceptor.Chain.class);
 
-    when(chain.request())
-        .thenReturn(new Request.Builder().url("http://1.1.1.1/heath-check").build());
+    RetrofitService retrofitService =
+        makeRetrofitService(wireMock.baseUrl(), spinnakerRequestHeaderInterceptor);
 
-    when(AuthenticatedRequest.getAuthenticationHeaders()).thenReturn(authHeaders);
+    // Add some spinnaker headers to the MDC
+    TEST_SPINNAKER_HEADERS.forEach(AuthenticatedRequest::set);
 
-    // when: "running the interceptor under test"
-    headerInterceptor.intercept(chain);
+    // Make a request
+    retrofitService.getRequest().execute();
 
-    // then: "the expected authorization header is added to the request before proceeding"
-    ArgumentCaptor<Request> requestCaptor = ArgumentCaptor.forClass(Request.class);
-    verify(chain).proceed(requestCaptor.capture());
-    Request request = requestCaptor.getValue();
-    assertThat(request.headers(Header.USER.getHeader())).isEqualTo(List.of("some user"));
-    assertThat(request.headers(Header.ACCOUNTS.getHeader())).isEqualTo(List.of("Some ACCOUNTS"));
+    // Verify that wiremock did/didn't receive the spinnaker headers as appropriate
+    TEST_SPINNAKER_HEADERS.forEach(
+        (Header header, String value) -> {
+          RequestPatternBuilder requestPatternBuilder =
+              getRequestedFor(urlPathEqualTo(REQUEST_PATH));
+          if (propagateSpinnakerHeaders) {
+            requestPatternBuilder.withHeader(header.getHeader(), equalTo(value));
+          } else {
+            requestPatternBuilder.withoutHeader(header.getHeader());
+          }
+          wireMock.verify(requestPatternBuilder);
+        });
+  }
+
+  private RetrofitService makeRetrofitService(
+      String baseUrl, SpinnakerRequestHeaderInterceptor spinnakerRequestHeaderInterceptor) {
+    OkHttpClient okHttpClient =
+        new OkHttpClient.Builder().addInterceptor(spinnakerRequestHeaderInterceptor).build();
+
+    return new Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .client(okHttpClient)
+        .build()
+        .create(RetrofitService.class);
+  }
+
+  interface RetrofitService {
+    @GET(REQUEST_PATH)
+    Call<Void> getRequest();
   }
 }
