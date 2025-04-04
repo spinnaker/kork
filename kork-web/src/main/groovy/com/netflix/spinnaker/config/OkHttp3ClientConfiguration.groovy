234e1d4714afa66c49f80a57379aa478bf3ab88d
David Byron <dbyron@salesforce.com>
2025-04-04 15:04:15 -0700

    doc(web): add comments to OkHttp3ClientConfiguration

--- kork-web/src/main/groovy/com/netflix/spinnaker/config/OkHttp3ClientConfiguration.groovy
+++ kork-web/src/main/groovy/com/netflix/spinnaker/config/OkHttp3ClientConfiguration.groovy
@@ -62,7 +62,7 @@ class OkHttp3ClientConfiguration {
   private final SpinnakerRequestHeaderInterceptor spinnakerRequestHeaderInterceptor
 
   /**
-   * {@link okhttp3.Interceptor} for correcting partial encoding done by Retrofit2.
+   * {@link okhttp3.Interceptor} for correcting partial encoding done by Retrofit2.  Do not use in retrofit1.
    */
   private final Retrofit2EncodeCorrectionInterceptor retrofit2EncodeCorrectionInterceptor
 
@@ -92,7 +92,7 @@ class OkHttp3ClientConfiguration {
   }
 
   /**
-   * @return OkHttpClient w/ <optional> key and trust stores
+   * @return OkHttpClient w/ <optional> key and trust stores.  For use with retrofit1.  Do not use with retrofit2.
    */
   OkHttpClient.Builder create() {
     if (okHttpClientConfigurationProperties.refreshableKeys.enabled) {
@@ -114,7 +114,8 @@ class OkHttp3ClientConfiguration {
   }
 
   /**
-   * @return OkHttpClient with SpinnakerRequestHeaderInterceptor as initial interceptor w/ <optional> key and trust stores
+   * @return OkHttpClient with SpinnakerRequestHeaderInterceptor and Retrofit2EncodeCorrectionInterceptor
+   * as initial interceptors w/ <optional> key and trust stores
    */
   OkHttpClient.Builder createForRetrofit2() {
     if (okHttpClientConfigurationProperties.refreshableKeys.enabled) {
